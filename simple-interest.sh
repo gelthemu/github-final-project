92dd6062417ece350688a6ee162f0e4584576d1a
Gel <121950929+gelthemu@users.noreply.github.com>
2023-10-09 23:02:25 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <gelthemu>
    # Input:
    # p, principal amount
    # t, time period in years
@@ -20,5 +20,3 @@
    s=`expr $p \* $t \* $r / 100`
    echo "The simple interest is: "
    echo $s
-
-# edited
